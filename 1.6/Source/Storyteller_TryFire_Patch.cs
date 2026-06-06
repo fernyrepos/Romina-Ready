4424e475e462efdd0b6e0266cc8638c053298d77
Taranchuk <nfhfyxer@gmail.com>
2026-06-06 16:30:28 +0300

    anomaly check

--- 1.6/Source/Storyteller_TryFire_Patch.cs
+++ 1.6/Source/Storyteller_TryFire_Patch.cs
@@ -13,7 +13,8 @@ namespace RominaReady
                 || fi.def.category == IncidentCategoryDefOf.DiseaseHuman
                 || fi.def.category == DefsOf.DiseaseAnimal
                 || fi.def.category == IncidentCategoryDefOf.DeepDrillInfestation
-                || (!RominaReadyMod.settings.allowSmallThreats && fi.def.category == IncidentCategoryDefOf.ThreatSmall);
+                || (!RominaReadyMod.settings.allowSmallThreats && fi.def.category == IncidentCategoryDefOf.ThreatSmall)
+                || fi.def.IsAnomalyIncident;
         }
 
         public static bool Prefix(FiringIncident fi, ref bool __result, out bool __state)
